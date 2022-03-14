150c491083e9d9692d2c5e5a4fc3bb206407d131
Jesus Ramirez <jesus.ramirez@copixil.com>
2022-03-14 16:47:03 -0600

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,28 @@
+const argv = require('yargs')
+                .option('b',{
+                    alias: 'base',
+                    type: 'number',
+                    demandOption: true,
+                    describe: 'Es la basde de la tabla de multiplicar'
+                })
+                .option('l',{
+                    alias: 'listar',
+                    type: 'boolean',
+                    default: false,
+                    describe: 'Muestra la tabla en consola'
+                })
+                .option('h',{
+                    alias: 'hasta',
+                    type: 'number',
+                    default: 10,
+                    describe: 'Limite de la base a multiplicar'
+                })
+                .check((argv, options)=>{
+                    if(isNaN(argv.b) || isNaN(argv.h)){
+                        throw 'La base tiene que se run numero';
+                    }
+                    return true;
+                })
+                .argv;
+
+module.exports = argv;
